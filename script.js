21d460d6134340d4f74dd0eeedf8c7bfa564d0e6
anabracadabra <146945705+anabracadabra@users.noreply.github.com>
2024-08-02 07:44:52 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaalternativa = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+const pergunta = [
+    {
+        enunciado:"pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+]
