33c38abadda9975216dfbc30fa917d0122440fff
anabracadabra <146945705+anabracadabra@users.noreply.github.com>
2024-10-10 08:13:08 -0300

    ....

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
-const caixaprincipal = document.querySelector('.caixa-principal');
-const caixapergunta = document.querySelector('.caixa-pergunta');
-const caixaalternativa = document.querySelector('.caixa-alternativa');
-const caixaresultado = document.querySelector('.caixa-resultado');
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
     {
-        enunciado:"Você gosta da inteligencia artificial?",
-        alternativas:[
+        enunciado: "Você gosta da ideia de Inteligência Artificial?",
+        alternativas: [
             {
-            texto:"SIM",
-            afirmativa:"afirmativa da alternativa 1"
-        },
-        {
-            texto:"NÃO",
-            afirmativa: "afirmativa da alternativa 2"
-        }
-            
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
         ]
     },
     {
-        enunciado:"Você credita que as pessoas estão ficando alienadas por conta das IA's?",
-        alternativas:[
+        enunciado: "Você utiliza frequentemente o ChatGpt?",
+        alternativas: [
             {
-            texto:"SIM",
-            afirmativa:"afirmativa da alternativa 1"
-        },
-        {
-            texto:"NÃO",
-            afirmativa: "afirmativa da alternativa 2"
-        }
-    ]
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
+        ]
+    },
+    {
+        enunciado: "Você acredita que a inteligência artificial pode superar a inteligência humana?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
+            }
+        ]
     }
-    
 ]
+
 let posicao = 0;
-let pergunta;
-function mostrapergunta(){
-    perguntaAtual=perguntas[posicao];
-    caixapergunta.textContent=perguntaAtual.enunciado;
+let perguntaAtual;
+let respostas = "";
+
+function mostraPergunta() {
+    if (posicao >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent=" ";
+    mostraAlternativas();
 }
-
-function mostraAlternativa(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas= document.createElement("button");
-        botaoAlternativas.textContent= alternativa.texto;
-        botaoAlternativas.addEventListener("click",function(){
-    
-        })
-
-        caixaAlternativa.appendChild(botaoAlterntivas);
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
+        caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
-mostrapergunta();
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmativa;
+    respostas = afirmacoes;
+    posicao++;
+    mostraPergunta();
+}
+function mostraResultado(){
+    
+}
+mostraPergunta();
